c3e758ee4229e834d3dd72b02ceb88fabea55aff
blast <rodfval.fv@gmail.com>
2021-12-15 18:14:06 +0100

    first commit

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+sudo mkdir -p /usr/local/bin
+sudo cp mailserverMegaGenerator /usr/local/bin/mailserverMegaGenerator 
+sudo cp mountMega /usr/local/bin/mountMega
+sudo cp umountMega /usr/local/bin/umountMega
+sudo cp mountMegaMerged /usr/local/bin/mountMegaMerged
+sudo cp umountMegaMerged /usr/local/bin/umountMegaMerged
+
+sudo cp rclone-mega@.service /lib/systemd/system/rclone-mega@.service
